5850aa69ecced0fce454cbce929d32f834808f23
Teagan Clayton <clay0063@algonquinlive.com>
2023-11-27 12:10:43 -0500

    Set up basic card format

--- FinalProject/ContentView.swift
+++ FinalProject/ContentView.swift
@@ -43,33 +43,69 @@ struct BuildingCards: View {
     var body: some View {
         let imageURL = data.image.replacingOccurrences(of: ".jpg", with: "")
         VStack {
-            Image(imageURL)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 300)
-            Text(data.name)
+            VStack {
+                Image(imageURL)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxHeight: 300)
+            }
+            VStack {
+                HStack {
+                    Image(systemName: "heart")
+                    Image(systemName: "square.and.arrow.up")
+                    Spacer()
+                    Text("NEW!").opacity(data.isNew ? 1 : 0)
+                }
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(data.name)
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.leading)
+                            .padding(.vertical, 2.0)
+                        HStack {
+                            Text(data.address)
+                            Spacer()
+                            Text("km from you")
+                        }
+                        .foregroundColor(Color.gray)
+                    }
+                }
+            }.padding()
         }
+        .frame(width: 350)
+        .foregroundStyle(.black)
+        .background(
+            Color.white
+                .shadow(radius: 2, x: 3, y: 3)
+        )
+        .padding()
+        
     }
 }
 
 struct BuildingDetailView: View {
     let data: Building
     var body: some View {
+        let imageURL = data.image.replacingOccurrences(of: ".jpg", with: "")
         ScrollView {
             VStack {
                 VStack{
                     Text("Main:")
+                    Image(imageURL, label: Text("\(data.imageDescription)"))
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 300)
                     Text("ID: \(data.id)")
                     Text("Name: \(data.name)")
                     Text("New: \(data.isNew ? "True" : "False")")
-                    Text("Address: \(data.address)")
                     Text("Description: \(data.description)")
                     Text("Website: \(data.website)")
                     Text("Category: \(data.category)")
-                    Text("Image: \(data.image)")
-                    Text("Image Desc: \(data.imageDescription)")
                     Text("Latitude: \(data.latitude)")
                     Text("Longitude: \(data.longitude)")
+                    Text("Address: \(data.address)")
                     Text("Category ID: \(data.categoryId)")
                 }.padding(.bottom)
                 
