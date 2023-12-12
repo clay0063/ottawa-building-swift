ba86837c911b2648ec43452b9a11aa9d3b8cfab1
Teagan Clayton <clay0063@algonquinlive.com>
2023-12-11 19:14:21 -0500

    Add filter button to map (visual)

--- FinalProject/Map/MapView.swift
+++ FinalProject/Map/MapView.swift
@@ -15,6 +15,12 @@ struct MapView: View {
     @State private var position: MapCameraPosition = .automatic
     @State private var selectedItem: Int?
     @State private var isSheetPresented = false
+    @State private var isFilterSheetPresented = false
+    @State private var searchName = ""
+    @State private var selectedCategory = "Placeholder"
+    @State private var selectedSortBy = "A-Z"
+    let filtering = BuildingFiltering()
+    @State private var selectedFeatures: Set<String> = []
     
     
     private var selectedPlace: Building? {
@@ -34,12 +40,33 @@ struct MapView: View {
                         longitude: building.longitude)
                 ).tag(building.id)
             }
-            //systemImage
+        }
+        .safeAreaInset(edge: .top) {
+            HStack {
+                Spacer()
+                HStack {
+                    Button {
+                        isFilterSheetPresented.toggle()
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: 45, height: 45)
+                                .foregroundStyle(.white.opacity(0.85))
+                                .shadow(color: .gray.opacity(0.5), radius: 5)
+                            
+                            Image("filterIcon")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 20.0)
+                        }
+                        
+                    }
+                    
+                }
+                .padding(.trailing, 5)
+            }
             
         }
-//        .onChange(of: selectedItem) {
-//            isSheetPresented.toggle()
-//        }
         .safeAreaInset(edge: .bottom) {
             if let selectedPlace {
                 //ADD close button
@@ -60,12 +87,14 @@ struct MapView: View {
             if let selectedPlace {
                 BuildingDetailView(data: selectedPlace, savedList: $savedList, dataList: listData)
                 //                    .presentationBackground(.thinMaterial)
-                    .padding(.horizontal)
                     .presentationDetents([.large])
                     .presentationCornerRadius(25)
             }
             
         })
+        .sheet(isPresented: $isFilterSheetPresented) {
+            FilterSheet(listData: listData, selectedCategory: $selectedCategory, selectedSortBy: $selectedSortBy, selectedFeatures: $selectedFeatures)
+        }
         .mapControls(){
             MapCompass()
             MapPitchToggle()
